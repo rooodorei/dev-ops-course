3e9baea2237bf92e4176a337e25aad3736842ca0
Joe <ddlzoe@hotmail.com>
2025-03-26 00:10:35 +0800

    add cors origin

--- bwic-svc/src/main/java/com/example/bwic_svc/controller/HiController.java
+++ bwic-svc/src/main/java/com/example/bwic_svc/controller/HiController.java
@@ -3,11 +3,14 @@ package com.example.bwic_svc.controller;
 import com.example.bwic_svc.Client;
 import com.example.bwic_svc.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
+@CrossOrigin(origins = "*") 
 public class HiController {
     @Autowired
     private ClientService clientService;
