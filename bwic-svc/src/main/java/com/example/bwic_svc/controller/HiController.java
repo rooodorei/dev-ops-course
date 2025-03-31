cef5c7ad6928d44c84c8aa0135f9636689cc3d7b
Joe <ddlzoe@hotmail.com>
2025-03-31 19:39:24 +0800

    add login endpoint

--- bwic-svc/src/main/java/com/example/bwic_svc/controller/HiController.java
+++ bwic-svc/src/main/java/com/example/bwic_svc/controller/HiController.java
@@ -4,8 +4,11 @@ import com.example.bwic_svc.Client;
 import com.example.bwic_svc.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
 
 @RestController
 public class HiController {
@@ -20,5 +23,16 @@ public class HiController {
     @GetMapping("/client/{id}")
     public Client getClient(@PathVariable Integer id) {
         return clientService.getClient(id);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody Map<String, String> map) {
+        String username = map.get("username");
+        String password = map.get("password");
+        if (username.equals("username") && password.equals("12345678")) {
+            return "success";
+        } else {
+            return "fail";
+        }
     }
 }
