b8352acbcc6fb34e411c5113274a45c776af5bf9
Joe <ddlzoe@hotmail.com>
2025-03-31 19:43:16 +0800

    add cross origin

--- bwic-svc/src/main/java/com/example/bwic_svc/controller/HiController.java
+++ bwic-svc/src/main/java/com/example/bwic_svc/controller/HiController.java
@@ -3,14 +3,12 @@ package com.example.bwic_svc.controller;
 import com.example.bwic_svc.Client;
 import com.example.bwic_svc.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.Map;
 
 @RestController
+@CrossOrigin("*")
 public class HiController {
     @Autowired
     private ClientService clientService;
